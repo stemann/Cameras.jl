9647a530afdc558e3b288b08b5d1d230d480e542
Jesper Stemann Andersen <jsa@ihp.dk>
2018-11-27 14:36:45 +0100

    Added documentation of basic acquisition functions

--- src/Cameras.jl
+++ src/Cameras.jl
@@ -10,9 +10,32 @@ export Camera,
 
 abstract type Camera end
 
+"""
+    isrunning(camera::Camera)
+
+Return if the camera is running.
+"""
 isrunning(camera::Camera) = error("No implementation for $(typeof(camera))")
+
+"""
+    start!(camera::Camera)
+
+Start camera, i.e. start image acquisition.
+"""
 start!(camera::Camera) = error("No implementation for $(typeof(camera))")
+
+"""
+    stop!(camera::Camera)
+
+Stop camera, i.e. stop image acquisition.
+"""
 stop!(camera::Camera) = error("No implementation for $(typeof(camera))")
+
+"""
+    take!(camera::Camera)
+
+Take an image, i.e. an [`AbstractArray`](@ref). Blocks until an image is available.
+"""
 take!(camera::Camera) = error("No implementation for $(typeof(camera))")
 
 import Base: iterate, IteratorSize
